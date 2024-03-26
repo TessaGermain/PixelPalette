ba93f90cfc1f1c3fa0abd33db13b9f51b0c54963
Tessa <tessa.germain2003@gmail.com>
2024-03-26 11:03:02 +0100

    login & submit

--- pixel-palette/src/Controller/UserController.php
+++ pixel-palette/src/Controller/UserController.php
@@ -19,16 +19,36 @@ class UserController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
+    #[Route('/index', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+
     #[Route('/login', name: 'login')]
     public function login(): Response
     {
-        return $this->render('user/login.html.twig');
-    }
+        return $this->render('user/login.html.twig', [
+            "error" => ""
+        ]);
+}
 
     #[Route('/login-form', name: 'login-form')]
-    public function loginForm(): Response
+    public function loginForm(Request $request, SessionInterface $session): Response
     {
-        return $this->render('user/login.html.twig');
+        $mail = $request->request->get('mail');
+        $password = $request->request->get('password');
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['mail' => $mail]);
+        if (password_verify($password, $user->getPassword())) {
+            $session->set('pseudo', $user->getPseudo());
+            $session->set('isLogin', true);
+            return $this->redirectToRoute('index');    
+        } else {
+            $session->clear();
+            return $this->render('user/login.html.twig', [
+                "error" => "Mot de passe incorrect"
+            ]);
+        }
     } 
 
     #[Route('/signup', name: 'signup')]
@@ -38,8 +58,19 @@ class UserController extends AbstractController
     }
 
     #[Route('/signup-form', name: 'signup-form')]
-    public function signupForm(): Response
+    public function signupForm(Request $request, SessionInterface $session): Response
     {
-        return $this->render('user/signup.html.twig');
+        $user = new User();
+        $mail = $request->request->get('mail');
+        $pseudo = $request->request->get('pseudo');
+        $password = $request->request->get('password');
+        $user->setMail($mail);
+        $user->setPseudo($pseudo);
+        $user->setPassword(password_hash($password, PASSWORD_DEFAULT));
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        $session->set('pseudo', $pseudo);
+        $session->set('isLogin', true);
+        return $this->redirectToRoute('index');
     }
 }
