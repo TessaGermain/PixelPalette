0a7bb5ad121ba5da67f3ab13bea332b2bcc4878a
Tessa <tessa.germain2003@gmail.com>
2024-03-26 16:02:13 +0100

    galery of author

--- pixel-palette/src/Controller/UserController.php
+++ pixel-palette/src/Controller/UserController.php
@@ -40,6 +40,7 @@ class UserController extends AbstractController
         $password = $request->request->get('password');
         $user = $this->entityManager->getRepository(User::class)->findOneBy(['mail' => $mail]);
         if (password_verify($password, $user->getPassword())) {
+            $session->set('id', $user->getId());
             $session->set('pseudo', $user->getPseudo());
             $session->set('isLogin', true);
             return $this->redirectToRoute('home');    
@@ -69,8 +70,33 @@ class UserController extends AbstractController
         $user->setPassword(password_hash($password, PASSWORD_DEFAULT));
         $this->entityManager->persist($user);
         $this->entityManager->flush();
-        $session->set('pseudo', $pseudo);
-        $session->set('isLogin', true);
-        return $this->redirectToRoute('home');
+        return $this->redirectToRoute('login');
+    }
+
+    #[Route('/authors', name: 'authors')]
+    public function authors(): Response
+    {
+        $authors = $this->entityManager->getRepository(User::class)->findAll();
+        $authorsAndPictures = [];
+        foreach ($authors as $author) {
+            $authorsAndPictures[$author->getId()]["id"] = $author->getId();
+            $authorsAndPictures[$author->getId()]["pseudo"] = $author->getPseudo();
+            $pictures = $author->getPictures();
+            $authorsAndPictures[$author->getId()]["numberPictures"] = count($pictures);
+        }
+        return $this->render('user/authors.html.twig', [
+            "authors" => $authorsAndPictures
+        ]);
+    }
+
+    #[Route('/author-pictures/{id}', name: 'author-pictures')]
+    public function authorPictures($id): Response
+    {
+        $author = $this->entityManager->getRepository(User::class)->findOneBy(['id' => $id]);
+        $pictures = $author->getPictures();
+        return $this->render('user/author-pictures.html.twig', [
+            "pictures" => $pictures,
+            "pseudo" => $author->getPseudo()
+        ]);
     }
 }
